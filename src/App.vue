b0e8aee26c1c20281a8016fdd8686d530db8fd20
Tak Castel <takcastel@gmail.com>
2019-03-11 15:23:39 +0100

    Update src files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello-world">
+    Hello {{ message }}
+  </div> 
+</template>
+
+<script>
+export default {
+  data: () => ({
+    message: 'world !'
+  })
+}
+</script>
+
+<style>
+  .hello-world {
+    padding: 16px;
+    color: red;
+  }
+</style>
