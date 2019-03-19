79419d889dd09c4c0439e25623d8319217944cb2
Tak Castel <takcastel@gmail.com>
2019-03-19 13:37:37 +0100

    Application codebase modifications

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="hello-world">
-    Hello {{ message }}
-  </div> 
+  <div class="app">
+    <header>
+      {{ title }}
+    </header>
+    <section>
+      {{ message }}
+    </section>
+    <footer>
+      {{ version }}
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    message: 'world !'
+    title: 'Mon application',
+    message: 'Hello world !',
+    version: '© 2018'
   })
 }
 </script>
 
 <style>
-  .hello-world {
-    padding: 16px;
-    color: red;
-  }
+body {
+  margin: 0;
+}
+
+.app {
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+
+.app > * {
+  padding: 16px;
+}
+
+section {
+  color: red;
+  flex: 1
+}
 </style>
